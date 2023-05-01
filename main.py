6baa437666f92024ab42f223298377c41cc00825
Gaurang Bharti <gaurangbharti@gmail.com>
2023-05-01 12:21:28 -0700

    Duckduckgo search

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain 
 from langchain.memory import ConversationBufferMemory
-from langchain.utilities import WikipediaAPIWrapper, ArxivAPIWrapper
+from langchain.tools import DuckDuckGoSearchRun
 
 apikey = str(st.secrets['OPENAI_KEY'])
 os.environ['OPENAI_API_KEY'] = apikey
@@ -22,30 +22,30 @@ title_template = PromptTemplate(
 )
 
 script_template = PromptTemplate(
-    input_variables = ['title', 'wikipedia_research'], 
-    template='write some tips on how to improve {title} while squatting while leveraging {wikipedia_research}'
+    input_variables = ['title', 'duckduckgo_search'], 
+    template='write some tips on how to improve {title} while squatting while leveraging {duckduckgo_search}'
 )
 
 # Memory 
 title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
 script_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
 
-
 # Llms
 llm = OpenAI(temperature=0.9) 
 title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
 script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script', memory=script_memory)
 
-wiki = WikipediaAPIWrapper()
-arxiv = ArxivAPIWrapper()
+search = DuckDuckGoSearchRun()
 # Show stuff to the screen if there's a prompt
 if prompt: 
     print("PROMPT: " + prompt)
     title = title_chain.run(prompt)
-    wiki_research = wiki.run(prompt) 
-    script = script_chain.run(title=title, wikipedia_research=wiki_research)
-
+    duckduckgo = search.run(f"how to improve {prompt}") 
+    script = script_chain.run(title=prompt, duckduckgo_search=duckduckgo)
+    
+    st.subheader(f"Some of the causes of {prompt}:")
     st.write(title) 
+    st.subheader("Tips on improving")
     st.write(script) 
 
     with st.expander('Title History'): 
@@ -54,5 +54,5 @@ if prompt:
     with st.expander('Script History'): 
         st.info(script_memory.buffer)
 
-    #with st.expander('Wikipedia Research'): 
-        #st.info(wiki_research)
+    with st.expander('DuckDuckGo Research'): 
+        st.info(duckduckgo)
